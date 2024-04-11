b87f224bc36f04e34f74255be762e65c8cf557b6
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-04-12 00:26:36 +0700

    :package: add uuid

--- src/llm/usecases.rs
+++ src/llm/usecases.rs
@@ -7,20 +7,17 @@ use async_trait::async_trait;
 use ollama_rs::Ollama;
 use qdrant_client::qdrant::PointStruct;
 use serde_json::json;
-use uuid::Uuid;
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 use tracing::{error, info};
+use uuid::Uuid;
 
 const EMBEDDINGS_MODEL: &str = "nomic-embed-text:latest";
 const COLLECTION: &str = "docs";
 
 #[async_trait]
 pub trait Usecases {
-    async fn doc_adding(
-        &self,
-        req: DocAddingReq,
-    ) -> Result<DocAddingSuccess, errors::DocAdding>;
+    async fn doc_adding(&self, req: DocAddingReq) -> Result<DocAddingSuccess, errors::DocAdding>;
 }
 
 #[derive(Clone)]
@@ -40,10 +37,7 @@ impl UsecasesImpl {
 
 #[async_trait]
 impl Usecases for UsecasesImpl {
-    async fn doc_adding(
-        &self,
-        req: DocAddingReq,
-    ) -> Result<DocAddingSuccess, errors::DocAdding> {
+    async fn doc_adding(&self, req: DocAddingReq) -> Result<DocAddingSuccess, errors::DocAdding> {
         let doc_embedded = &self
             .ollama
             .generate_embeddings(EMBEDDINGS_MODEL.to_string(), req.clone().doc, None)
@@ -53,11 +47,8 @@ impl Usecases for UsecasesImpl {
                 errors::DocAdding
             })?;
 
-        let doc_embedded_vec: Vec<f32> = doc_embedded
-            .embeddings
-            .iter()
-            .map(|&x| x as f32)
-            .collect();
+        let doc_embedded_vec: Vec<f32> =
+            doc_embedded.embeddings.iter().map(|&x| x as f32).collect();
 
         let payload = json!({
             "doc": req.doc,
